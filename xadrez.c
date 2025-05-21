a96ea5e2feb9b88393dd5439baa4b6c76a32a25b
felipe <felipsss2004@gmail.com>
2025-05-21 11:17:39 +0000

    Novato

--- xadrez.c
+++ xadrez.c
@@ -5,28 +5,36 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+//DECLARAÇÃO DAS VARIÁVEIS
+    int bispo;
+    int rainha;
+    int torre;
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+//MOVIMENTANDO CADA PEÇA
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+printf("BEM VINDO AO JOGO DE XADREZ!!!\n");
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+for (bispo = 0; bispo < 5; bispo++){
+    printf("BISPO : Avante e Para Direita\n"); //imprime direção da peça bispo estrutura for
+}
+printf("----------------------------------------------------------------------\n");
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+while (rainha<8)
+{
+    printf("Rainha: Esquerda\n", rainha); //imprime direção da peça rainha estrutura while
+    rainha++;                            
+}
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+printf("-----------------------------------------------------------------------\n");
+
+do
+{
+    printf("Torre: Direita\n",torre);   //imprime direção da peça torre estrutura do while
+    torre++;
+} while (torre<5);
+
+
 
     return 0;
 }
